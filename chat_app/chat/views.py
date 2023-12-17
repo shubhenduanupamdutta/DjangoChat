b7ab5ad88c3444d50435c87cc64742c4c9c894f5
ShubhenduADutta <shubhendu@flexli.in>
2023-12-17 21:42:33 +0530

    Show available chat rooms on homepage

--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import ChatRoom
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    chat_rooms = ChatRoom.objects.all()
+    return render(request, 'chat/index.html', {"chat_rooms": chat_rooms})
