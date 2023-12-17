b41a4a18b6144fb5ac8af5f5c180beb6b2f4ea3f
ShubhenduADutta <shubhendu@flexli.in>
2023-12-17 21:47:29 +0530

    Create a detail view for individual chat room and link it to homepage.

--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -6,3 +6,8 @@ from .models import ChatRoom
 def index(request):
     chat_rooms = ChatRoom.objects.all()
     return render(request, 'chat/index.html', {"chat_rooms": chat_rooms})
+
+
+def chat_room(request, slug):
+    chat_room = ChatRoom.objects.get(slug=slug)
+    return render(request, 'chat/room.html', {"chat_room": chat_room})
