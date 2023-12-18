d4472c7a3bf9d8c43e5b2e72d4b4977d5200b450
ShubhenduADutta <shubhendu@flexli.in>
2023-12-18 10:11:11 +0530

    Register ChatMessage model to admin.

--- chat_app/chat/admin.py
+++ chat_app/chat/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import ChatRoom
+from .models import ChatMessage, ChatRoom
 
 
 # Register your models here.
 admin.site.register(ChatRoom)
+admin.site.register(ChatMessage)
