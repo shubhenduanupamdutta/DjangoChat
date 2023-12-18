e60680594b93926b3de67a497de81aad6290731a
ShubhenduADutta <shubhendu@flexli.in>
2023-12-18 10:11:50 +0530

    Create functionality to save chat message in database.

--- chat_app/chat/consumers.py
+++ chat_app/chat/consumers.py
@@ -1,6 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from .models import ChatRoom, ChatMessage
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -9,7 +11,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f"chat_{self.room_name}"
 
-        await self.channel_layer.group_add(
+        await self.channel_layer.group_add(  # type: ignore
             self.room_group_name,
             self.channel_name
         )
@@ -17,7 +19,42 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self):
-        await self.channel_layer.group_discard(
+        await self.channel_layer.group_discard(  # type: ignore
             self.channel_name,
             self.room_group_name
         )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.channel_layer.group_send(  # type: ignore
+            self.room_group_name, {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+                'room': room,
+            }
+        )
+
+        await self.save_message(username, room, message)
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'room': room,
+        }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = ChatRoom.objects.get(slug=room)
+
+        ChatMessage.objects.create(user=user, room=room, message_content=message)
